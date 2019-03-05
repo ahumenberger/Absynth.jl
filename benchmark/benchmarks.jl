1c1396b3da88499643c0eadeaf70b8d7d832a7c2
Andreas Humenberger <ahumenberger@gmail.com>
2019-03-05 14:48:40 +0100

    Benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,15 +8,44 @@ macro sbench(suite, args...)
     end)
 end
 
-@template freire(a, b) = begin
-    x = x + a
-    y = y + b
+# while x>r
+#     x = x-r
+#     r = r+1
+# end
+@template freire1(a,b,c) = begin
+    x = x + a*r + b
+    r = r + c
 end
+
+# while x-s > 0
+#     x = x-s
+#     s = s+6*r+3
+#     r = r+1
+# end
+@template freire2(a,b,c,d,e,f) = begin
+    x = x + a*s + b*r + c
+    s = s + d*r + e
+    r = r + f
+end
+
+# while n<=N
+#     n = n+1
+#     x = x+y
+#     y = y+z
+#     z = z+6
+# end
+@template cohencu(a,b,c,d,e,f,g,h,i,j) = begin
+    n = n + a
+    x = x + b*n + c
+    y = y + d*x + e*n + f
+    z = z + g*y + h*x + i*n + j
+end
+
 
 const group = BenchmarkGroup()
 
-@sbench group freire (x == 3y) (b>0 && x_0>1)
-@sbench group freire (x == 3y && x==y) (b>0 && x_0>1)
+@sbench group freire1 (x == 3y) (b>0 && x_0>1)
+@sbench group freire1 (x == 3y && x==y) (b>0 && x_0>1)
 
 tune!(group)
 results = run(group)
