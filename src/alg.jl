71d3831e5326c30bc0da089a318a7e7c5e87d05a
Andreas Humenberger <ahumenberger@gmail.com>
2019-04-30 12:05:36 +0200

    Update

--- src/alg.jl
+++ src/alg.jl
@@ -38,15 +38,30 @@ function constraints(B::Matrix{Basic}, inv::Basic)
     end
 end
 
+abstract type AlgebraicNumber end
+
 function constraints(B::Matrix{Basic}, inv::Basic, ms::Vector{Int})
-    rs = symroot(length(ms))
+    t = length(ms)
+    rs = symroot(t)
     lc = Basic("n")
     cfs = cforms(size(B, 1), rs, ms, lc)
     p = SymEngine.lambdify(inv)(cfs...)
-    cscfs = cstr_cforms(B, rs, ms)
+    cscforms = cstr_cforms(B, rs, ms)
     csroots, distinct = cstr_roots(B, rs, ms)
     csrel = cstr_algrel(p, rs, lc)
-    @info "" cscfs csroots distinct csrel
+    @info "" cscforms csroots distinct csrel
+
+    cs = Iterators.flatten(symconst(i, j, size(B, 1)) for i in 1:t for j in 1:ms[i]) |> collect
+    bs = Iterators.flatten(B) |> collect
+    vars = [cs; bs; rs]
+    varmap = Dict(v=>AlgebraicNumber for v in vars)
+    @info "Variables" vars |> collect
+
+    solver = T()
+    NLSat.variables!(solver, varmap...)
+    NLSat.constraints!(solver, cscforms...)
+    NLSat.constraints!(solver, csroots...)
+    NLSat.constraints!(solver, csrel...)
 end
 
 function cforms(varcnt::Int, rs::Vector{Basic}, ms::Vector{Int}, lc::Basic)
@@ -56,6 +71,44 @@ end
 
 symconst(i::Int, j::Int, rows::Int) = reshape([Basic("c$i$j$k") for k in 1:rows], rows, 1)
 symroot(n::Int) = [Basic("w$i") for i in 1:n]
+
+# ------------------------------------------------------------------------------
+
+# function solve(s::Synthesizer; solvertype::Type{T}=Z3Solver, timeout::Int=120) where {T<:NLSolver}
+#     @assert !isempty(invariants(s)) "No invariants specified."
+#     if s.updated
+#         _constraints!(s)
+#     end
+
+#     solver = T()
+
+#     tmpl = template(s)
+#     ivars = Dict{Symbol,Type}(Symbol(Recurrences.initvar(k))=>v for (k,v) in varmap(tmpl))
+#     NLSat.variables!(solver, argmap(tmpl)..., ivars...)
+#     if !isempty(constraints(s))
+#         NLSat.constraints!(solver, constraints(s)...)
+#     end
+#     if !isempty(invconstraints(s))
+#         NLSat.constraints!(solver, invconstraints(s)...)
+#     end
+
+#     status, model = NLSat.solve(solver, timeout=timeout)
+#     @debug "Result of $T" status model
+#     if status == NLSat.sat
+#         undef = setdiff(args(tmpl), keys(model))
+#         if !isempty(undef)
+#             # TODO: deal with unknowns which do not appear in model
+#             for v in undef
+#                 push!(model, v => 0)
+#             end
+#         end
+#         return setvalues(tmpl, Dict{Symbol,CoeffType}(model))
+#     elseif status == NLSat.timeout
+#         @error "Timeout"
+#     else
+#         @error "No solution exists. Result of $T: $status"
+#     end
+# end
 
 # ------------------------------------------------------------------------------
 
