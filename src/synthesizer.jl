ced45f7a46f1d234630b67f1062a0c47902aeed8
Andreas Humenberger <ahumenberger@gmail.com>
2019-05-15 10:20:18 +0200

    MultiSynthesizer for enumerating solutions

--- src/synthesizer.jl
+++ src/synthesizer.jl
@@ -21,14 +21,16 @@ mutable struct Solutions
     status::NLStatus
     body::Matrix{Basic}
     vars::Vector{Basic}
+    timeout::Int # seconds
 
-    Solutions(s::NLSolver, b::Matrix{Basic}, vars::Vector{Basic}) = new(s, NLSat.sat, b, vars)
+    Solutions(s::NLSolver, b::Matrix{Basic}, vars::Vector{Basic}, timeout::Int) =
+        new(s, NLSat.sat, b, vars, timeout)
 end
 
 iterate(it::Solutions) = iterate(it, 0)
 
 function iterate(it::Solutions, state)
-    it.status, model = NLSat.solve(it.solver)
+    it.status, model = NLSat.solve(it.solver, it.timeout)
     if it.status == NLSat.sat
         body = [model[Symbol(string(b))] for b in it.body]
         init = [model[Symbol(string(b))] for b in initvec(size(it.body, 1))]
@@ -37,6 +39,8 @@ function iterate(it::Solutions, state)
     return nothing
 end
 
+reason(s::Solutions) = s.status
+
 # ------------------------------------------------------------------------------
 
 struct Synthesizer{T<:NLSolver}
@@ -44,6 +48,7 @@ struct Synthesizer{T<:NLSolver}
     polys::Vector{Basic}
     roots::Combinatorics.IntegerPartitions # multiplicities of roots
     vars::Vector{Basic}
+    timeout::Int # seconds
 
     function Synthesizer(::Type{T}, polys::Vector{Expr}, shape::Symbol) where {T<:NLSolver}
         ps = map(Basic, polys)
@@ -52,7 +57,7 @@ struct Synthesizer{T<:NLSolver}
     
         dims = length(fs)
         body = dynamicsmatrix(dims, shape)
-        new{T}(body, ps, partitions(dims), fs)
+        new{T}(body, ps, partitions(dims), fs, 5)
     end
 end
 
@@ -69,7 +74,7 @@ function next(s::Synthesizer{T}, next) where {T}
         solver = T()
         NLSat.variables!(solver, varmap)
         NLSat.constraints!(solver, cstr)
-        return Solutions(solver, s.body, s.vars), state
+        return Solutions(solver, s.body, s.vars, s.timeout), state
     end
     return nothing
 end
@@ -77,3 +82,44 @@ end
 synth(x::Expr) = synth(Yices, [x])
 synth(t::Type{T}, polys::Vector{Expr}) where {T<:NLSolver} =
     Synthesizer(t, polys, :F)
+
+# ------------------------------------------------------------------------------
+
+synth(t::Type{T}, polys::Vector{Expr}, timeout::Int, maxsol::Int) where {T<:NLSolver} =
+    MultiSynthesizer(Synthesizer(t, polys, :F), maxsol)
+
+struct MultiSynthesizer{T<:NLSolver}
+    synth::Synthesizer{T}
+    maxsol::Int
+end
+
+iterate(ms::MultiSynthesizer) = first_sol(ms, iterate(ms.synth))
+
+function iterate(ms::MultiSynthesizer, state)
+    (synth_state, solutions, sol_state) = state
+    if sol_state >= ms.maxsol
+        next = iterate(ms.synth, synth_state)
+        return first_sol(ms, next)
+    else
+        next_sol = iterate(solutions, sol_state)
+        if next_sol !== nothing
+            (loop, sol_state) = next_sol
+            return loop, (synth_state, solutions, sol_state)
+        else
+            return reason(solutions), (synth_state, solutions, ms.maxsol)
+        end
+    end
+end
+
+function first_sol(ms::MultiSynthesizer, next)
+    if next !== nothing
+        (solutions, synth_state) = next
+        next_sol = iterate(solutions)
+        if next_sol !== nothing
+            (loop, sol_state) = next_sol
+            return loop, (synth_state, solutions, sol_state)
+        else
+            return reason(solutions), (synth_state, solutions, ms.maxsol)
+        end
+    end
+end
