29e952814250e4c5c3c361a475726ff89fee2d5c
Andreas Humenberger <ahumenberger@gmail.com>
2019-05-18 14:30:27 +0200

    Minor

--- src/report.jl
+++ src/report.jl
@@ -3,18 +3,18 @@ using ProgressMeter
 
 const InvSet = Vector{Expr}
 
-function report(polys::Vector{InvSet}; solvers=[Yices,Z3], timeout=5, maxsol=3)
-    p = Progress(length(polys)*length(solvers)*maxsol)
+function report(polys::Vector{InvSet}; solvers=[Yices,Z3], timeout=2, maxsol=3)
+    prog = Progress(length(polys)*length(solvers)*maxsol)
     df = DataFrame(Solver = Type{<:NLSolver}[], Polys = Vector{Basic}[], Roots = Vector{Int}[], Loop = Union{Loop,NLStatus}[])
     for solver in solvers
         for invset in polys
             solutions = synth(solver, invset, timeout, maxsol)
             for s in solutions
                 push!(df, (s.info.solver, s.info.polys, s.info.roots, s.result))
-                next!(p)
+                next!(prog)
             end
         end
     end
-    finish!(p)
+    finish!(prog)
     df
 end
