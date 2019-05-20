5797b66d42d038114556524df1002d4bfe88f3d3
Andreas Humenberger <ahumenberger@gmail.com>
2019-05-20 12:08:10 +0200

    Pretty print

--- src/show.jl
+++ src/show.jl
@@ -3,38 +3,37 @@ to_assignments(xs::Vector, ys::Vector) = ["$x = $y" for (x,y) in zip(xs,ys)]
 to_lines(xs::Vector{String}, indent::Int) = join(xs, "\n$(repeat("    ", indent))")
 to_list(xs) = join(xs, ", ")
 
-
-function Base.summary(io::IO, l::Loop)
-    print(io, "Loop for loop invariants (")
-    # print(io, join(string.(invariants(s)), ","))
-    # print(io, ")")
-end
-
-# function show(io::IO, l::Loop)
-#     print(io, """
-#         $(to_lines(to_assignments(l.vars, l.init), 1))
-#         while true
-#             $(to_lines(to_assignments(l.vars, l.body*l.vars), 2))
-#         end
-#     """)
-# end
+Base.show(io::IO, s::NLStatus) = print(io, lowercase(string(s)))
 
 function Base.show(io::IO, l::Loop)
-    # compact = get(io, :compact, false)
+    compact = get(io, :compact, false)
 
-    # if compact
+    if compact
         show(io, l.body)
-    # else
-        # print(io, """
-        # $(to_list(l.info.vars)) = $(to_list(l.init))
-        # while true
-        #     $(to_list(l.info.vars)) = $(to_list(l.body*l.info.vars))
-        # end
-        # """)
-    # end
+    else
+        h = size(l.body, 1)
+        lstr, rstr = lpar(h), rpar(h, " ")
+        body = sprint(Base.print_matrix, l.body)
+        init = sprint(Base.print_matrix, l.init)
+        str = mergestr(space(h, "\t"), lstr, body, rstr, space(h, "\t"), lstr, init, rstr)
+        print(io, str)
+    end
 end
 
 function Base.show(io::IO, ::MIME"text/plain", m::Loop)
     println("Synthesized loop:")
     show(io, m)
+end
+
+lpar(h::Int, d = "") = h == 1 ? "($(d)" : join(["⎛$(d)"; fill("⎜$(d)", h-2); "⎝$(d)"], "\n")
+rpar(h::Int, d = "") = h == 1 ? "$(d))" : join(["$(d)⎞"; fill("$(d)⎟", h-2); "$(d)⎠"], "\n")
+
+space(h::Int, sp=" ") = join(fill(sp, h), "\n")
+
+function mergestr(strings::String...)
+    splits = split.(strings, "\n")
+    rows = length(splits[1])
+    cols = length(splits)
+    matr = reshape(collect(Iterators.flatten(splits)), rows, cols)
+    join([join(matr[i,:]) for i in 1:size(matr, 1)], "\n")
 end
