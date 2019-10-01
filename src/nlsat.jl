b5db1a5e6e5c6b6745507095eb863f34ff334a3d
Andreas Humenberger <ahumenberger@gmail.com>
2019-10-01 08:59:24 +0200

    Finalize MathematicaSolver

--- src/nlsat.jl
+++ src/nlsat.jl
@@ -309,32 +309,39 @@ typename(x::PyObject) = x.__class__.__name__
 # function solve(s::MathematicaSolver; timeout::Int=-1)
 #     formula = MathLink.parseexpr(_tostring(s.cs))
 #     vars = MathLink.parseexpr(string("{", join(collect(keys(s.vars)), ", "), "}"))
-#     timeout = -1
 #     result = if timeout <= 0
-#         W"FindInstance"(formula, vars)
+#         W"FindInstance"(formula, vars, W"Algebraics")
 #     else
-#         W"TimeConstrained"(W"FindInstance"(formula, vars), timeout)
+#         W"TimeConstrained"(W"FindInstance"(formula, vars, W"Algebraics"), timeout, W"Timeout")
 #     end
+#     start = time_ns()
 #     result = weval(result)
+#     elapsed = Millisecond(round((time_ns()-start)/1e6))
 #     res = _to_julia(result)
-
-#     if isempty(res)
-#         return NLSat.unsat
+#     if res == :Timeout
+#         return NLSat.timeout, Second(timeout), nothing
+#     elseif res == :Aborted
+#         @warn("Mathematica aborted")
+#         return NLSat.unknown, elapsed, nothing
+#     elseif isempty(res)
+#         return NLSat.unsat, elapsed, nothing
 #     end
-#     NLSat.sat, Second(1), Dict(first(res))
+#     NLSat.sat, elapsed, Dict(first(res))
 # end
 
 # _to_julia(w::MathLink.WSymbol) = Symbol(w.name)
 # _to_julia(w::Number) = w
 # function _to_julia(w::MathLink.WExpr)
-#     @info "" w.head w.args
 #     if w.head == W"List"
 #         return [_to_julia(x) for x in w.args]
 #     elseif w.head == W"Rule"
 #         return _to_julia(w.args[1]) => _to_julia(w.args[2])
 #     elseif w.head == W"Rational"
 #         return _to_julia(w.args[1]) // _to_julia(w.args[2])
+#     elseif w.head == W"Complex"
+#         return _to_julia(w.args[1]) + _to_julia(w.args[2]) * im
 #     end
+#     w
 # end
 
 end # module
