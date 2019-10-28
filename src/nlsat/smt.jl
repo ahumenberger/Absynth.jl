8cbb212ce77c5c3a460233fcd30130df5a40eb1e
Andreas Humenberger <ahumenberger@gmail.com>
2019-10-28 20:18:45 +0100

    Restructure

--- src/nlsat/smt.jl
+++ src/nlsat/smt.jl
@@ -73,7 +73,7 @@ function write_problem(io::IO, s::T) where {T<:NLSolver}
     write_footer(io, s)
 end
 
-function solve(s::T; timeout::Int=-1) where {T<:NLSolver}
+function solve(s::SMTSolver; timeout::Int=-1)
     path, io = mktemp()
     write_problem(io, s)
     close(io)
@@ -85,3 +85,68 @@ function solve(s::T; timeout::Int=-1) where {T<:NLSolver}
         rm(newpath)
     end
 end
+
+# ------------------------------------------------------------------------------
+
+function openproc(parse::Function, cmd::Cmd; timeout=-1)
+    start = time_ns()
+    P = open(pipeline(cmd, stderr=devnull))
+    if timeout < 0
+        wait(P)
+    else
+        timedwait(()->!process_running(P), float(timeout))
+        if process_running(P)
+            @debug "Kill"
+            kill(P)
+            close(P.in)
+            return NLSat.timeout, Second(timeout), nothing
+        end
+    end
+    elapsed = Millisecond(round((time_ns()-start)/1e6))
+
+    if P.exitcode >= 0
+        lines = readlines(P)
+        status = popfirst!(lines)
+        if status == "sat"
+            d = parse(lines)
+            return NLSat.sat, elapsed, d
+        elseif status == "unsat"
+            return NLSat.unsat, elapsed, nothing
+        elseif status == "unknown"
+            return NLSat.unknown, elapsed, nothing
+        end
+
+        error("Unknown status: $status")
+    end
+    return NLSat.unknown, elapsed, nothing
+end
+
+function parse_output_smt(s::Array{String})
+    m = NLModel()
+    sexpr = parse_sexpr(join(s))
+    for x in sexpr.vec
+        @assert length(x.vec) == 2
+        sym = x.vec[1]
+        val = try_int(eval(to_expr(x.vec[2])))
+        push!(m, sym=>val)
+    end
+    m
+end
+
+function parse_output_yices(s::Array{String})
+    m = NLModel()
+    sexpr = parse_sexpr(string("(", join(s), ")"))
+    for x in sexpr.vec
+        @assert length(x.vec) == 3 && x.vec[1] == :(=) "$x"
+        sym = x.vec[2]
+        val = try_int(eval(to_expr(x.vec[3])))
+        push!(m, sym=>val)
+    end
+    m
+end
+
+to_expr(x) = x isa SExpr ? Expr(:call, map(to_expr, x.vec)...) : x
+
+try_int(x) = isinteger(x) ? convert(Int, x) : rationalize(x)
+
+Base.rationalize(x::Rational) = x
