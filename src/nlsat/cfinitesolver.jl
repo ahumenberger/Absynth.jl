cf39f53e8bb42212d34475975aae21ad46e9c6bf
Andreas Humenberger <ahumenberger@gmail.com>
2020-03-11 08:03:35 +0100

    Change return of maxsat

--- src/nlsat/cfinitesolver.jl
+++ src/nlsat/cfinitesolver.jl
@@ -35,22 +35,6 @@ function variables!(s::CFiniteSolver, d::Dict{Symbol,Type})
     push!(s.vars, d...)
 end
 
-# function preprocess_clauseset(cs::ClauseSet)
-#     cl_cfin = []
-#     cl_norm = ClauseSet()
-#     for cl in cs
-#         # @info "" cl length(cl) typeof(cl) == CFiniteConstraint{EQ}
-#         if length(cl) == 1 && first(cl) isa CFiniteConstraint{EQ}
-#             push!(cl_cfin, first(cl))
-#         elseif length(cl) == 1 && cl isa CFiniteConstraint{NEQ}
-#             cl_norm &= expand(cl)
-#         else
-#             cl_norm &= cl
-#         end
-#     end
-#     cl_norm, cl_cfin
-# end
-
 function find_partition(model::Model, ms)
     ps = Dict{Z3Expr,Vector{Int}}()
     for (i, m) in enumerate(ms)
@@ -78,20 +62,15 @@ end
 function learn(s::Solver, ms, us, i)
     zero = real_val(ctx(s), 0)
     x = sum(ite(ms[i] == ms[j], us[j], zero) for j in 1:length(ms))
-    @info "Learn" x
+    @debug "Learn" x
     add(s, x == 0)
 end
 
-function check_clauses(s::Solver, mss, uss)
-    model = get_model(s)
-    @info "" model
+function check_clauses(s::Solver, model, mss, uss)
     for (ms, us) in zip(mss, uss)
         ps = find_partition(model, ms)
         violated = check_partition(model, us, ps)
         if violated !== nothing
-            # @info "" ms us collect(ps) violated
-            # @info assertions(s)
-            # TODO: remove violated from us?
             learn(s, ms, us, violated)
             return false
         end
@@ -99,19 +78,18 @@ function check_clauses(s::Solver, mss, uss)
     return true
 end
 
-function check_cfinite(s::CFiniteSolver, z3::Solver, vars)
-    uss = [[Z3Expr(ctx(z3), vars, x) for x in c[1].us] for c in s.cfin_clauses]
-    mss = [[Z3Expr(ctx(z3), vars, x) for x in c[1].ms] for c in s.cfin_clauses]
+function check_cfinite(s::CFiniteSolver, z3::Solver, vars, soft_clauses)
+    uss = [[Z3Expr(ctx(z3), vars, x) for x in c[1].us] for c in soft_clauses]
+    mss = [[Z3Expr(ctx(z3), vars, x) for x in c[1].ms] for c in soft_clauses]
 
     soft_clauses = Z3Expr[u == 0 for u in Iterators.flatten(uss)]
-    @info "before" soft_clauses
+    @debug "Soft clauses" soft_clauses
 
     max_it = length(soft_clauses)
     for _ in 1:max_it
-        res, _ = maxsat(z3, soft_clauses)
-        @info "" soft_clauses
+        res, model = maxsat(z3, soft_clauses)
         res != Z3.sat && return res # hard clauses not satisfiable or timeout
-        sat = check_clauses(z3, mss, uss)
+        sat = check_clauses2(z3, model, mss, uss)
         sat && return Z3.sat
     end
     Z3.unsat
@@ -121,6 +99,8 @@ Tactic(s::String) = Z3.Tactic(main_ctx(), s)
 
 function mk_solver()
     set_param("unsat_core", true)
+    # set_param("verbose", 2)
+
     t = Tactic("simplify") & Tactic("propagate-values") & Tactic("solve-eqs") &
         Tactic("purify-arith") & Tactic("elim-term-ite") & Tactic("simplify") & Tactic("tseitin-cnf") &
         Tactic("nlsat")
@@ -143,7 +123,16 @@ function solve(s::CFiniteSolver; timeout::Int=-1)
         add(z3, Z3Expr(ctx, z3vars, cl))
     end
 
-    elapsed_sec = @elapsed res = check_cfinite(s, z3, z3vars)
+    sclauses = ClauseSet()
+    for cl in s.cfin_clauses
+        if length(cl[1].us) == 1
+            add(z3, Z3Expr(ctx, z3vars, cl[1].us[1]) == 0)
+        else
+            push!(sclauses, cl)
+        end
+    end
+
+    elapsed_sec = @elapsed res = check_cfinite(s, z3, z3vars, sclauses)
     elapsed = Millisecond(round(elapsed_sec*1000))
     if res == Z3.sat
         return NLSat.sat, elapsed, parse_model(get_model(z3))
--- src/nlsat/maxsat.jl
+++ src/nlsat/maxsat.jl
@@ -1,23 +1,3 @@
-# def add_def(s, fml):
-#     name = Bool("%s" % fml)
-#     s.add(name == fml)
-#     return name
-
-# def relax_core(s, core, Fs):
-#     prefix = BoolVal(True)
-#     Fs -= { f for f in core }
-#     for i in range(len(core)-1):
-#         prefix = add_def(s, And(core[i], prefix))
-#         Fs |= { add_def(s, Or(prefix, core[i+1])) }
-
-# def maxsat(s, Fs):
-#     cost = 0
-#     Fs0 = Fs.copy()
-#     while unsat == s.check(Fs):
-#         cost += 1
-#         relax_core(s, s.unsat_core(), Fs)    
-#     return cost, { f for f in Fs0 if tt(s, f) }
-
 function add_def(s::Solver, x::Z3Expr)
     name = bool_const(ctx(s), "$(id(x))")
     add(s, name == x)
@@ -26,41 +6,30 @@ end
 
 function relax_core(s::Solver, core::ExprVector, cs::Vector{<:Z3Expr})
     prefix = bool_val(ctx(s), true)
-    @info "" core
     for c in core
         deleteat!(cs, findfirst(isequal(c), cs))
     end
     for i in 1:length(core)-1
         prefix = add_def(s, and(core[i], prefix))
-        # cs = Z3Expr[or(c, add_def(s, or(prefix, core[i+1]))) for c in cs]
         push!(cs, add_def(s, or(prefix, core[i+1])))
     end
     cs
 end
 
 function maxsat(s::Solver, cs::Vector{<:Z3Expr})
-    # res = check(s)
-    # @info "after"
-    # res != Z3.sat && return res, nothing
     cost = 0
     cs0 = copy(cs)
-    while (res = check(s, cs)) == Z3.unsat && cost < length(cs0)
-        @info "" res
+    cs1 = copy(cs)
+    push(s)
+    while (res = check(s, cs1)) == Z3.unsat && cost < length(cs0)
         cost += 1
-        cs = relax_core(s, unsat_core(s), cs)
-        @info "" cs
+        cs1 = relax_core(s, unsat_core(s), cs1)
     end
-    @info "Maxsat" cost res==Z3.sat reason_unknown(s)
-    # @info "Maxsat" res == 
-    # open("/Users/ahumenberger/Downloads/valtest/absynth.smt2", "w") do io
-    #     @info "Check sat" check(s, cs)
-    #     push(s)
-    #     for x in cs
-    #         add(s, x)
-    #     end
-    #     @info "Check sat" check(s, cs)
-    #     write(io, to_smt2(s, "sat"))
-    # end
-    res, [c for c in cs0 if is_true(Z3.eval(get_model(s), c, false))]
+    @debug "Maxsat" cost res==Z3.sat reason_unknown(s)
+    satisfied = [c for c in cs0 if is_true(Z3.eval(get_model(s), c, false))]
+    m = get_model(s)
+    pop(s, 1)
+    @debug "Maxsat" satisfied m
+    res, m
 end
 
