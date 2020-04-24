c2932a54424e0c69fcaf5ddb3b057925a7b32684
Andreas Humenberger <ahumenberger@gmail.com>
2020-04-24 09:17:14 +0200

    First steps towards branch and bound solver

--- src/nlsat/cfinitesolver.jl
+++ src/nlsat/cfinitesolver.jl
@@ -1,9 +1,3 @@
-using Z3
-# : real_const, or, add, check
-
-
-const Z3Expr = Z3.Expr
-
 include("maxsat.jl")
 
 mutable struct CFiniteSolver <: NLSolver
@@ -16,7 +10,7 @@ mutable struct CFiniteSolver <: NLSolver
     end
 end
 
-function constraints!(s::CFiniteSolver, cs::ClauseSet)
+function add!(s::CFiniteSolver, cs::ClauseSet)
     for c in cs
         if length(c) == 1 && first(c) isa CFiniteConstraint{EQ}
             s.cfin_clauses &= c
@@ -29,11 +23,10 @@ function constraints!(s::CFiniteSolver, cs::ClauseSet)
         end
     end
 end
-constraints_soft!(s::CFiniteSolver, cs::ClauseSet) = s.soft_clauses &= cs
+add_soft!(s::CFiniteSolver, cs::ClauseSet) = s.soft_clauses &= cs
+add_vars!(s::CFiniteSolver, d::Dict{Symbol,Type}) = push!(s.vars, d...)
 
-function variables!(s::CFiniteSolver, d::Dict{Symbol,Type})
-    push!(s.vars, d...)
-end
+# ------------------------------------------------------------------------------
 
 function find_partition(model::Model, ms)
     ps = Dict{Z3Expr,Vector{Int}}()
@@ -48,34 +41,118 @@ function find_partition(model::Model, ms)
     values(ps)
 end
 
-function check_partition(model::Model, us, ps)
-    zero = real_val(ctx(model), 0)
-    for subset in ps
-        s = sum(us[i] for i in subset)
-        if !is_true(Z3.eval(model, s == zero, false))
-            return first(i for i in subset if is_true(Z3.eval(model, us[i] != zero, false)))
-        end
-    end
-    nothing # all partitions are satisfied
+const Partition = Vector{Vector{Int}}
+
+g_partition_history = Vector{Partition}()
+g_solutions = Vector{Partition}()
+
+
+function move_iterator(P::Partition, set_idx, elem)
+    # elem = P[set_idx][elem_idx]
+    P = deepcopy(P)
+    deleteat!(P[set_idx], findfirst(x->x==elem, P[set_idx]))
+    iter = ([
+        (if i == j
+            [Y; elem]
+        else
+            Y
+        end) for (j, Y) in enumerate(P)
+    ] for i = 1:length(P) if i != set_idx)
+    (i for i in iter if !(i in g_partition_history))
 end
 
-function learn(s::Solver, ms, us, i)
-    zero = real_val(ctx(s), 0)
-    x = sum(ite(ms[i] == ms[j], us[j], zero) for j in 1:length(ms))
-    @debug "Learn" x
-    add(s, x == 0)
+function learn_partitions(ms, us, partition, partition_map, core)
+    @info "Learn next partitions" string(partition) partition_map core
+    
+        
+        Iterators.flatten((if is_eq(x)
+            # @info "" partition_map[id(x)]
+            (p1, i1), (p2, i2) = partition_map[id(x)]
+            # index1, index2 = partition[p1][i1], partition[p2][i2]
+            it1 = move_iterator(partition, p1, i1)
+            it2 = move_iterator(partition, p2, i2)
+            Iterators.flatten((it1, it2))
+        elseif is_distinct(x)
+
+        else
+            error("No equality/inequality: $(x)")
+        end for x in core))
 end
 
-function check_clauses(s::Solver, model, mss, uss)
-    for (ms, us) in zip(mss, uss)
-        ps = find_partition(model, ms)
-        violated = check_partition(model, us, ps)
-        if violated !== nothing
-            learn(s, ms, us, violated)
-            return false
+function solve_constraints(s::Solver, mss, uss, partitions, current=1)
+    push(s)
+    ms, us, partition = mss[current], uss[current], partitions[current]
+    assumptions = Z3Expr[]
+    partition_map = Dict{UInt32, Pair{Pair{Int,Int},Pair{Int,Int}}}()
+    for (t, indices) in enumerate(partition)
+        usum = us[indices[1]]
+        for i = 2:length(indices)
+            j, k = indices[i-1], indices[i]
+            ass = (ms[j] == ms[k])
+            push!(assumptions, ass)
+            usum += us[k]
+            @assert !haskey(partition_map, id(ass))
+            partition_map[id(ass)] = (t=>j) => (t=>k)
+        end
+        add(s, usum == 0)
+        if t > 1
+            j, k = first(partition[t-1]), first(indices)
+            ass = ms[j] != ms[k]
+            push!(assumptions, ass)
+            @assert !haskey(partition_map, id(ass))
+            partition_map[id(ass)] = (t-1=>j) => (t=>k)
         end
     end
-    return true
+
+    # x = Base.unique(Iterators.flatten(collect(Iterators.product(X, X)) for X in Iterators.product(partition...)))
+    # xx = []
+    # for (i, (a, b)) in enumerate(x)
+    #     @info "" a b a == b (b,a) in x
+    #     if a != b && !((b, a) in xx)
+    #         push!(xx, (a,b))
+    #         ass = ms[a] != ms[b]
+    #         push!(assumptions, ass)
+    #         # @assert !haskey(partition_map, id(ass))
+    #         # partition_map[id(ass)] = (t-1=>j) => (t=>k)
+    #     end
+    # end
+
+    @debug "Assumptions" assumptions
+    res = check(s, assumptions)
+    push!(g_partition_history, partition)
+    if res == Z3.sat
+        if current == length(mss)
+            # for (i,(m,u)) in enumerate(zip(mss[1], uss[1]))
+            #     @info "Term $i" m u
+            # end
+            # @info "" assumptions get_model(s)
+            for a in assumptions
+                add(s, a)
+            end
+            # println(s)
+            @info "" check(s) == Z3.sat get_model(s)
+            # @info "" get_model(s) mss uss
+            return true
+        end
+        for a in assumptions
+            add(s, a)
+        end
+        @info "Next constraint" s
+        return solve_constraints(s, mss, uss, partitions, current+1)
+    elseif res == Z3.unsat
+        @info "UNSAT"
+        for ps in learn_partitions(ms, us, partition, partition_map, unsat_core(s))
+            # @info "" ps
+            parts = deepcopy(partitions)
+            parts[current] = ps
+            # @info "Next partition" s
+            pop(s, 1)
+            solve_constraints(s, mss, uss, parts, current) && return true
+        end
+        return false
+    else
+        error("Z3 unknown: $(reason_unknown(s))")
+    end
 end
 
 function check_cfinite(s::CFiniteSolver, z3::Solver, vars, soft_clauses)
@@ -85,54 +162,42 @@ function check_cfinite(s::CFiniteSolver, z3::Solver, vars, soft_clauses)
     soft_clauses = Z3Expr[u == 0 for u in Iterators.flatten(uss)]
     @debug "Soft clauses" soft_clauses
 
-    max_it = length(soft_clauses)
-    for _ in 1:max_it
-        res, model = maxsat(z3, soft_clauses)
-        res != Z3.sat && return res # hard clauses not satisfiable or timeout
-        sat = check_clauses2(z3, model, mss, uss)
-        sat && return Z3.sat
-    end
+    res, model = maxsat(z3, soft_clauses)
+    res != Z3.sat && return res # hard clauses not satisfiable or timeout
+    partitions = [collect(find_partition(model, ms)) for ms in mss]
+    solve_constraints(z3, mss, uss, partitions) && return Z3.sat
     Z3.unsat
 end
 
-Tactic(s::String) = Z3.Tactic(main_ctx(), s)
-
-function mk_solver()
-    set_param("unsat_core", true)
-    # set_param("verbose", 2)
-
-    t = Tactic("simplify") & Tactic("propagate-values") & Tactic("solve-eqs") &
-        Tactic("purify-arith") & Tactic("elim-term-ite") & Tactic("simplify") & Tactic("tseitin-cnf") &
-        Tactic("nlsat")
-    Z3.mk_solver(t)
-end
-
 function solve(s::CFiniteSolver; timeout::Int=-1)
+    set_param("unsat_core", true)
     ctx = main_ctx()
     if timeout > 0
         set(ctx, "timeout", timeout*1000)
     end
+    z3 = mk_solver()
+    # add variables
     z3vars = Dict{Symbol,Z3Expr}()
     for (k,v) in s.vars
         push!(z3vars, k=>real_const(ctx, string(k)))
     end
-    
-    # z3 = Solver(ctx, "QF_NRA")
-    z3 = mk_solver()
+    # add hard clauses
     for cl in s.hard_clauses
         add(z3, Z3Expr(ctx, z3vars, cl))
     end
-
+    # C-finite clauses
     sclauses = ClauseSet()
     for cl in s.cfin_clauses
-        if length(cl[1].us) == 1
-            add(z3, Z3Expr(ctx, z3vars, cl[1].us[1]) == 0)
+        # if clause consists of just one term then add it as a hard clause
+        if length(first(cl).us) == 1
+            add(z3, Z3Expr(ctx, z3vars, first(cl).us[1]) == 0)
         else
             push!(sclauses, cl)
         end
     end
 
     elapsed_sec = @elapsed res = check_cfinite(s, z3, z3vars, sclauses)
+    @debug "CFiniteSolver" res == Z3.sat reason_unknown(z3)
     elapsed = Millisecond(round(elapsed_sec*1000))
     if res == Z3.sat
         return NLSat.sat, elapsed, parse_model(get_model(z3))
@@ -146,48 +211,3 @@ function solve(s::CFiniteSolver; timeout::Int=-1)
         return NLSat.unknown, elapsed, nothing
     end
 end
-
-function parse_model(m::Model)
-    nlmodel = NLModel()
-    for (k,v) in consts(m)
-        sym = Symbol(string(k))
-        if is_int(v)
-            push!(nlmodel, sym=>Int(v))
-        elseif is_real(v)
-            push!(nlmodel, sym=>try_int(Rational{Int}(v)))
-        elseif is_algebraic(v)
-            approx = get_decimal_string(v, 3)
-            @warn "Algebraic numbers not yet supported, got $(v), returning approximation $(approx)"
-            push!(nlmodel, sym=>approx)
-        end
-    end
-    @info "" nlmodel
-    nlmodel
-end
-
-# ------------------------------------------------------------------------------
-
-function Z3Expr(ctx::Context, vs::Dict{Symbol,Z3Expr}, ex::XExpr)
-    t = postwalk(ex) do x
-        if x isa Float64
-            # if isinteger(x)
-            #     convert(Int, x)
-            # else
-                y = rationalize(x)
-                real_val(ctx, numerator(y), denominator(y))
-            # end
-        elseif x isa Int
-            real_val(ctx, x)
-        elseif issymbol(x)
-            get(vs, x, x)
-        else
-            x
-        end
-    end
-    eval(t)
-end
-
-Z3Expr(ctx::Context, vs::Dict{Symbol,Z3Expr}, c::Constraint{EQ}) = Z3Expr(ctx, vs, c.poly) == 0
-Z3Expr(ctx::Context, vs::Dict{Symbol,Z3Expr}, c::Constraint{NEQ}) = Z3Expr(ctx, vs, c.poly) != 0
-Z3Expr(ctx::Context, vs::Dict{Symbol,Z3Expr}, c::Clause) =
-    length(c) > 1 ? or(Z3Expr(ctx, vs, x) for x in c) : Z3Expr(ctx, vs, first(c))
